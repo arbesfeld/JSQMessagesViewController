0a8c7b3264c4a213770cb41a5863db155aab79f5
Matthew Arbesfeld <arbesfeld@gmail.com>
2015-01-25 17:55:28 -0500

    Fix text vertical center issue

--- JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
+++ JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
@@ -466,10 +466,10 @@ const CGFloat kJSQMessagesCollectionViewAvatarSizeDefault = 30.0f;
         
         //  add extra 2 points of space, because `boundingRectWithSize:` is slightly off
         //  not sure why. magix. (shrug) if you know, submit a PR
-        CGFloat verticalInsets = verticalContainerInsets + verticalFrameInsets + 2.0f;
+        CGFloat verticalInsets = verticalContainerInsets + verticalFrameInsets;
         
         //  same as above, an extra 2 points of magix
-        CGFloat finalWidth = MAX(stringSize.width + horizontalInsetsTotal, self.bubbleImageAssetWidth) + 2.0f;
+        CGFloat finalWidth = MAX(stringSize.width + horizontalInsetsTotal, self.bubbleImageAssetWidth);
         
         finalSize = CGSizeMake(finalWidth, stringSize.height + verticalInsets);
     }
